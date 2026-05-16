f875494b3e60f3d167325bc3d90a9c63ffa5c3f8
Johnp21-ss <algarjohn21@gmail.com>
2026-05-16 19:42:30 -0300

    Update planilha.js

--- api/planilha.js
+++ api/planilha.js
@@ -1,18 +1,23 @@
+import jwt from "jsonwebtoken";
+
 export default async function handler(req, res) {
 
-  const token = req.headers.authorization;
+  const auth = req.headers.authorization;
 
-  if (token !== "Bearer SEGREDO_123") {
-    return res.status(401).send("Não autorizado");
-  }
+  if (!auth) return res.status(401).send("Sem token");
+
+  const token = auth.split(" ")[1];
 
   try {
+    const decoded = jwt.verify(token, "SEGREDO_SUPER_FORTE");
+
     const response = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-PWDRlSYT4cY9rOK7Dz1R2Fe0HzoXFbu4icqnPk5M6eGOBUngZFwkhI-9TXPpT9d-wvWCzXNVxteq/pub?gid=498256387&single=true&output=csv");
+
     const csv = await response.text();
 
-    res.status(200).send(csv);
+    return res.status(200).send(csv);
 
-  } catch (error) {
-    res.status(500).send("Erro ao buscar dados");
+  } catch {
+    return res.status(401).send("Token inválido");
   }
 }
